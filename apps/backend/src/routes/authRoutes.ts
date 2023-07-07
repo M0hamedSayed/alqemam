d39fffaeb0ef326fd494405ba75e89dedc92a7db
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2023-07-08 00:59:33 +0300

    feat(router): ✨ add new routes

--- apps/backend/src/routes/authRoutes.ts
+++ apps/backend/src/routes/authRoutes.ts
@@ -1,18 +1,25 @@
 import express, { Router } from 'express';
-import { registerValidations } from '../validations/authValidatons';
+import { loginValidations, registerValidations } from '../validations/authValidatons';
 import { validatorErrors } from '../middlewares/validatorErrors';
-import { sendVerificationEmail, signupPost } from '../controllers/signup';
+import { sendVerificationEmail, sendWelcomeMailSocial, signupPost } from '../controllers/signup';
 import { init } from '../common/utils/passport';
 import config from '../common/config/env-config';
+import { emailVerify, loginPost, logoutGet, sendWelcomeMail } from '../controllers/login';
+import { validateToken } from '../controllers/validateToken';
 const authRouter: Router = express.Router();
 
 authRouter.post('/signup', registerValidations, validatorErrors, signupPost, sendVerificationEmail);
+authRouter.post('/login', loginValidations, validatorErrors, loginPost, sendWelcomeMail);
+authRouter.get('/user/verify/:id/:otp', emailVerify);
+authRouter.get('/me', validateToken);
+authRouter.get('/logout', logoutGet);
 
 // facebook auth
 authRouter.get(
   '/auth/facebook',
   init().authenticate('facebook', {
     failureRedirect: `${config.baseUrl}/auth/failure`,
+    successRedirect: `${config.frontUrl}/auth/success`,
     scope: [
       'email',
       'public_profile',
@@ -32,6 +39,7 @@ authRouter.get(
   init().authenticate('facebook', {
     session: false,
   }),
+  sendWelcomeMailSocial,
 );
 
 export default authRouter;
