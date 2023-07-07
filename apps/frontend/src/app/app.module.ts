c98eeb99a797abf91f70feda17a5a4292874bcf3
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2023-07-08 02:54:57 +0300

    feat(routes): ✨ config routes

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -4,14 +4,29 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgToastModule } from 'ng-angular-popup';
+import { HttpClientModule } from '@angular/common/http';
+import { CrudModule } from './pages/crud/crud.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotfoundModule } from './pages/notfound/notfound.module';
+import { VerifyEmailModule } from './pages/verifyEmail/verify-email.module';
+import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    NgToastModule,
+    HttpClientModule,
+    CrudModule,
+    SharedModule,
+    VerifyEmailModule,
+    NotfoundModule,
+    BrowserAnimationsModule,
+    RouterModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
-  providers: [],
+  providers: [{ provide: 'baseURL', useValue: 'https://localhost:3000/' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
+/* eslint-disable @typescript-eslint/typedef */
 import { Route } from '@angular/router';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { VerifyEmailComponent } from './pages/verifyEmail/verify-email.component';
+import { NotAuthGuard } from './guards/not-auth.guard';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./pages/home/home.module').then(p => p.HomeModule),
+  },
+  { path: 'task1', loadChildren: () => import('./pages/register/register.module').then(p => p.RegisterModule) },
+  { path: 'task2', component: CrudComponent },
+  { path: 'user/verify/:id/:otp', component: VerifyEmailComponent, canActivate: [NotAuthGuard] },
+
+  { path: '**', component: NotfoundComponent },
+];
