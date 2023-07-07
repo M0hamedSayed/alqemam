1481ba6b683c82ebd22ced00d52811cc599ea314
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2023-07-08 02:54:07 +0300

    feat(app): ✨ add global preloader with routing

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { Observable, filter, map, of } from 'rxjs';
 
 @Component({
   selector: 'alqemam-task-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  loading$: Observable<boolean> = of(false);
+  constructor(private _router: Router) {}
+
+  ngOnInit() {
+    this.loading$ = this._router.events.pipe(
+      filter(
+        (e: any) =>
+          e instanceof NavigationStart ||
+          e instanceof NavigationEnd ||
+          e instanceof NavigationCancel ||
+          e instanceof NavigationError,
+      ),
+      map((e: any) => e instanceof NavigationStart),
+    );
+  }
 }
