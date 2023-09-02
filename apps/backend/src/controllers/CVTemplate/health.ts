056a47e49836403cdbb6f6db5e5d57fd1ebb8f96
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2023-09-02 17:57:52 +0300

    chore(nx): 🔧 update eslint

--- apps/backend/src/controllers/CVTemplate/health.ts
+++ apps/backend/src/controllers/CVTemplate/health.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const healthCheck = (_request: Request, response: Response, _next: NextFunction) => {
+  console.log('test');
+
   response.status(200).json({
     success: true,
     message: 'App is up & running.',
